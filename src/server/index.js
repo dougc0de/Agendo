4dfb159a20fa30f6948342e262f64ce7c8cebf70
Douglas Espinoza <de575836@gmail.com>
2026-04-16 16:45:51 -0500

    Rotacion de info

--- src/server/index.js
+++ src/server/index.js
@@ -25,6 +25,7 @@ app.use("/auth", authRoutes);
 app.use("/pacientes", pacienteRoutes);
 app.use("/reservas", reservaRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
